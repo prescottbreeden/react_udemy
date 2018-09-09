fcb6a57d91f277120877b232f24d321a9e3e5d7b
prescottbreeden <prescott.breeden@gmail.com>
2018-09-09 16:36:04 -0700

    react basics, event listeners, two-way data binding, styling

---
+++ basics/src/App.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+	state = {
+		persons: [
+			{ name: 'bob', age: 34 },
+			{ name: 'chuck', age: 28 },
+			{ name: 'bugs', age: 50 },
+			{ name: 'dick', age: 90 },
+			{ name: 'jane', age: 22 },
+			{ name: 'anita', age: 27 }
+		],
+		otherState: 'something else of value'
+	};
+
+	switchNameHandler = (newName) => {
+		this.setState({
+			persons: [
+				{ name: newName, age: 34 },
+				{ name: newName, age: 28 },
+				{ name: newName, age: 50 },
+				{ name: newName, age: 90 },
+				{ name: newName, age: 22 },
+				{ name: newName, age: 27 }
+			]
+		});
+	};
+
+	nameChangedHandler = (event) => {
+		this.setState({
+			persons: [
+				{ name: event.target.value, age: 34 },
+				{ name: event.target.value, age: 28 },
+				{ name: event.target.value, age: 50 },
+				{ name: event.target.value, age: 90 },
+				{ name: event.target.value, age: 22 },
+				{ name: event.target.value, age: 27 }
+			]
+		});
+	};
+
+  render() {
+		const style = {
+			backgroundColor: 'steelblue',
+			font: 'inherit',
+			border: 'none',
+			padding: '18px 50px',
+			color: 'white',
+			borderRadius: '200px'
+		};
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+				<h2>rubber baby buggy bumpers</h2>
+				<button 
+					style={style}
+					onClick={() => this.switchNameHandler('dingo')}>
+					Switch Name
+				</button>
+				<Person 
+					changed={this.nameChangedHandler}
+					name={this.state.persons[0].name} 
+					age={this.state.persons[0].age}>
+					My hobbies: Racing
+				</Person>
+				<Person 
+					changed={this.nameChangedHandler}
+					name={this.state.persons[1].name} 
+					age={this.state.persons[1].age}
+					click={this.switchNameHandler.bind(this, 'Norris!')}>
+					My hobbies: Kicking
+				</Person>
+				<Person 
+					changed={this.nameChangedHandler}
+					name={this.state.persons[2].name} 
+					age={this.state.persons[2].age}>
+					My hobbies: Eating Carrots
+				</Person>
+				<Person 
+					changed={this.nameChangedHandler}
+					name={this.state.persons[3].name} 
+					age={this.state.persons[3].age}>
+					My hobbies: Asshole
+				</Person>
+				<Person 
+					changed={this.nameChangedHandler}
+					name={this.state.persons[4].name} 
+					age={this.state.persons[4].age}>
+					My hobbies: Sucking
+				</Person>
+				<Person 
+					changed={this.nameChangedHandler}
+					name={this.state.persons[5].name} 
+					age={this.state.persons[5].age}>
+					My hobbies: Fucking
+				</Person>
+      </div>
+    );
+  };
+};
+
+export default App;
--- basics/src/Person/Person.js
+++ basics/src/Person/Person.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import './Person.css';
 
 const person = (props) => {
 	return (
 		<div className="person">
 			<p>I'm a person... whatever that is...</p>
-			<p>I think my name is {props.name}</p>
+			<p onClick={props.click}>I think my name is {props.name}</p>
 			<p>Apparently I'm {Math.floor(Math.random() * 30)} years old even though I thought I was {props.age}...</p>
+			<p>{props.children}</p>
+			<input type='text' onChange={props.changed} value={props.name} />
 		</div>
 	);
 }
