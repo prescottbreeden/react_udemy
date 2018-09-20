49a758a850c0ada15f9f46afa7a2acbb74adb1ce
prescottbreeden <prescott.breeden@gmail.com>
2018-09-20 01:11:00 -0700

    converted person and persons to stateful components to play with lifecycle hooks

--- component_dive/src/components/Persons/Person/Person.js
+++ component_dive/src/components/Persons/Person/Person.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Person.css';
 
-const person = (props) => {
 
-	return (
-		<div className="person">
-			<p onClick={props.click}>I think my name is {props.name}</p>
-			<p>Apparently I'm {Math.floor(Math.random() * 30)} years old even though I thought I was {props.age}...</p>
-			<input type='text' onChange={props.changed} value={props.name} />
-		</div>
-	);
+class Person extends Component {
+	render() {
+		return (
+			<div className="person">
+				<p onClick={this.props.click}>I think my name is {this.props.name}</p>
+				<p>Apparently I'm {Math.floor(Math.random() * 30)} years old even though I thought I was {this.props.age}...</p>
+				<input type='text' onChange={this.props.changed} value={this.props.name} />
+			</div>
+		);
+	}
 }
 
 
-export default person;
+export default Person;
--- component_dive/src/components/Persons/Persons.js
+++ component_dive/src/components/Persons/Persons.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
 
-			return (
-					<Person 
+class Persons extends Component {
+	render() {
+		return this.props.persons.map((person, index) => {
+			return <Person 
 						key={person.id}	
 						name={person.name} 
 						age={person.age}
-						click={() => props.clicked(index)}
-						changed={(event) => props.changed(event, person.id)}
-					/>
-			);
-		})
+						click={() => this.props.clicked(index)}
+						changed={(event) => this.props.changed(event, person.id)} />
+		});
+	}
+}
 
-export default persons;
+
+export default Persons;
